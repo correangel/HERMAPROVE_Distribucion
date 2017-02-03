9cb57b1502d694d334ec2bd1d5970a0582930e44
eslozano <eslozano@espol.edu.ec>
2017-02-02 22:58:10 -0500

    Actualizar la carpeta root

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8080/hermaprove/upload/admin/');
-define('HTTP_CATALOG', 'http://localhost:8080/hermaprove/upload/');
+define('HTTP_SERVER', 'http://localhost:8080/hermaprove/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/hermaprove/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8080/hermaprove/upload/admin/');
-define('HTTPS_CATALOG', 'http://localhost:8080/hermaprove/upload/');
+define('HTTPS_SERVER', 'http://localhost:8080/hermaprove/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/hermaprove/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/wampx64/www/hermaprove/upload/admin/');
-define('DIR_SYSTEM', 'C:/wampx64/www/hermaprove/upload/system/');
-define('DIR_IMAGE', 'C:/wampx64/www/hermaprove/upload/image/');
-define('DIR_LANGUAGE', 'C:/wampx64/www/hermaprove/upload/admin/language/');
-define('DIR_TEMPLATE', 'C:/wampx64/www/hermaprove/upload/admin/view/template/');
-define('DIR_CONFIG', 'C:/wampx64/www/hermaprove/upload/system/config/');
-define('DIR_CACHE', 'C:/wampx64/www/hermaprove/upload/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/wampx64/www/hermaprove/upload/system/storage/download/');
-define('DIR_LOGS', 'C:/wampx64/www/hermaprove/upload/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/wampx64/www/hermaprove/upload/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/wampx64/www/hermaprove/upload/system/storage/upload/');
-define('DIR_CATALOG', 'C:/wampx64/www/hermaprove/upload/catalog/');
+define('DIR_APPLICATION', 'C:/wampx64/www/hermaprove/admin/');
+define('DIR_SYSTEM', 'C:/wampx64/www/hermaprove/system/');
+define('DIR_IMAGE', 'C:/wampx64/www/hermaprove/image/');
+define('DIR_LANGUAGE', 'C:/wampx64/www/hermaprove/admin/language/');
+define('DIR_TEMPLATE', 'C:/wampx64/www/hermaprove/admin/view/template/');
+define('DIR_CONFIG', 'C:/wampx64/www/hermaprove/system/config/');
+define('DIR_CACHE', 'C:/wampx64/www/hermaprove/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wampx64/www/hermaprove/system/storage/download/');
+define('DIR_LOGS', 'C:/wampx64/www/hermaprove/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wampx64/www/hermaprove/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wampx64/www/hermaprove/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wampx64/www/hermaprove/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
